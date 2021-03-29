ef4995732233d4936f20315afc5823b68a5df691
hugo.M <hugo.mencoboni@gmail.com>
2021-03-28 22:05:39 -0400

    Init

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using CommandLine;
+using System;
+
+namespace CliParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<CommandeLineArgumentsModel>(args)
+                      .WithParsed(model =>
+                      {
+                          Console.WriteLine($"Nombre: -n {model.Nombre}");
+                          Console.WriteLine($"String: -s {model.String}");
+                      });
+        }
+
+        public class CommandeLineArgumentsModel
+        {
+            [Option('n', "numberTest", Required = false)]
+            public int Nombre { get; set; }
+
+            [Option('s', "stringTest", Required = true, HelpText = "stringTest (-s) argument must be provided")]
+            public string String { get; set; }
+        }
+    }
+}
